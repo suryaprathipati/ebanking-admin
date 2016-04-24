e48d8c344d11175a0a487d9016b296e1ad195c19
suryaprathipati <surya@MacBook-Air.local>
2016-04-24 11:24:47 +0530

    initial upload

---
+++ src/main/java/org/kexim/ebanking/ebanking_admin/App.java
@@ -0,0 +1,13 @@
+package org.kexim.ebanking.ebanking_admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
